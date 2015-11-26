15cf3c3218d780812e95ae58983ae9c49b3e7ef9
OlgaGladyo <o.gladyo.99@gmail.com>
2015-11-26 15:00:06 +0200

    9.8.new

--- 9/9.8.cpp
+++ 9/9.8.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int MinDivisor(int n){
-	
-	for(int i = 2; i <= sqrt(n); i++){
-		if (n % i == 0){
-			return i;
-		}
-	}
-	
-return n;
+
+int divisor(int n)
+{
+    int i;
+    for (i=2; i <= sqrt(n); i++)
+    {
+        if (n%i == 0) return i;
+    }
+    return n;
 }
-int main() {
-	int n;
-	cin >> n;
-	cout << MinDivisor(n);
-	return 0;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << endl << divisor(n) << endl;
+    return 0;
 }
